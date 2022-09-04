bf393a1a51d6ef9f6a1fe0a442d81553b6544b4f
Your Name <nghilt19411@gmail.com>
2022-09-04 11:05:05 +0700

    update

--- Project/Container/src/App.js
+++ Project/Container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from './components/MarketingApp';
 export default () =>{
     return (
         <div>
-            <h1>Nghi</h1>
+            <h1>Le Thanh Nghi</h1>
             <hr/>
             <MarketingApp/>
         </div>
